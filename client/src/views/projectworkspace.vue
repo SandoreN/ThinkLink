0360f45645240c487515411cb3d865ff58d31040
--get <kuklinskij1@southernct.edu>
2024-04-15 03:48:12 -0400

    project_workspace .js page includes markdown editor.

--- client/src/views/projectworkspace.vue
+++ client/src/views/projectworkspace.vue
@@ -1,38 +1,75 @@
 <template>
-  <div class="projectworkspace-container"
-    ><div class="projectworkspace-container1"
-      ><app-header1></app-header1
-      ><div class="projectworkspace-body"
-        ><app-leftsidebar></app-leftsidebar
-        ><div class="projectworkspace-pagemain"></div
-        ><div class="projectworkspace-projectsidebar"
-          ><div class="projectworkspace-container2"></div
-          ><div class="projectworkspace-container3"></div
-          ><div class="projectworkspace-container4"></div
-          ><div class="projectworkspace-container5"></div></div></div></div
-  ></div>
+  <div class="projectworkspace-container">
+    <div class="projectworkspace-container1">
+      <app-header rootClassName="header-root-class-name5"></app-header>
+      <div class="projectworkspace-body">
+        <app-leftsidebar></app-leftsidebar>
+        <div class="projectworkspace-pagemain">
+          <input class="draft-name-input" v-model="draftName" placeholder="Enter draft name" />
+          <div v-html="compiledMarkdown" class="markdown-preview"></div>
+          <textarea id="markdown-editor"></textarea>
+          <savedraftbuttoncontainer @click="saveDraft"></savedraftbuttoncontainer>
+        </div>
+        <app-rightsidebar></app-rightsidebar>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import AppHeader1 from '../components/header1'
+import AppHeader from '../components/header'
 import AppLeftsidebar from '../components/leftsidebar'
+import AppRightsidebar from '../components/rightsidebar'
+import EasyMDE from 'easymde'
+import Savedraftbuttoncontainer from '../components/savedraftbuttoncontainer'
+import * as marked from 'marked';
 
 export default {
   name: 'Projectworkspace',
-  props: {},
+  data() {
+    return {
+      draftName: '',
+      mde: null,
+    }
+  },
   components: {
-    AppHeader1,
+    AppHeader,
     AppLeftsidebar,
+    AppRightsidebar,
+    Savedraftbuttoncontainer,
   },
-  metaInfo: {
-    title: 'projectworkspace - ThinkLink',
-    meta: [
-      {
-        property: 'og:title',
-        content: 'projectworkspace - ThinkLink',
-      },
-    ],
+  mounted() {
+    this.$nextTick(() => {
+      this.mde = new EasyMDE({
+        element: document.getElementById('markdown-editor'),
+        toolbar: [
+          "bold", 
+          "italic", 
+          "heading", "|", 
+          "quote",
+          "unordered-list",
+          "ordered-list", "|",
+          "link",
+          "image", "|",
+          "guide"
+        ],      
+      })
+      this.mde.codemirror.refresh()
+      })
   },
+  methods: {
+    saveDraft() {
+      const draftContent = this.mde.value()
+      // Implement your saving logic here. For example:
+      // this.$http.post('/api/saveDraft', { name: this.draftName, content: draftContent })
+      console.log(this.draftName, draftContent)
+    },
+  },
+  computed: {
+    compiledMarkdown() {
+      return this.mde ? marked.marked(this.mde.value()) : '';
+    }
+  }
 }
 </script>
 
@@ -58,6 +95,7 @@ export default {
   flex-direction: column;
   justify-content: flex-start;
 }
+
 .projectworkspace-body {
   flex: 1;
   width: 100%;
@@ -68,15 +106,24 @@ export default {
   flex-direction: row;
   justify-content: flex-start;
 }
+
 .projectworkspace-pagemain {
-  flex: 1;
-  width: 200px;
-  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  padding-top: 30px;
+  width: 800px;
+  height: 1000px; 
+  margin: 0 auto; 
   position: relative;
-  align-self: stretch;
-  align-items: flex-start;
-  flex-direction: column;
+
 }
+
+#markdown-editor {
+  width: 100%; /* fill the parent div */
+  height: 100%; /* fill the parent div */
+}
+
 .projectworkspace-projectsidebar {
   flex: 0 0 auto;
   width: 225px;
@@ -126,5 +173,23 @@ export default {
   border-style: dashed;
   border-width: 1px;
   justify-content: center;
+}
+.draft-name-input {
+  height: 55px; /* Adjust the height as needed */
+  padding: 10px;
+  font-size: 30px; /* Adjust the font size as needed */
+  margin-bottom: 20px;
+  border: 2px solid black;
+  border-radius: 4px;
+  align-items: center;
+  justify-content: center;
+  /* Add other styling properties as needed */
+}
+.markdown-preview {
+  margin-top: 10px; /* Add some space above the preview pane */
+  margin-bottom: 10px; /* Add some space below the preview pane */
+  overflow-y: auto; /* Add a scrollbar if the content is too long to fit in the pane */
+  height: 200px;
+  border: 1px solid black;
 }
 </style>
