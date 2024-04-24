f1c7dba9bed0da41e8ee664f1ae2807bd69400bc
jescobar <escobarj6@southernct.edu>
2024-04-24 19:53:32 -0400

    last time?

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -3,7 +3,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from dotenv import load_dotenv
 
-<<<<<<< HEAD
 db = SQLAlchemy()
 
 
@@ -27,34 +26,6 @@ app.register_blueprint(routes_bp)
 
 
 
-=======
-load_dotenv()  # This loads the environment variables from a .env file
-
-
-# Create the Flask application
-app = Flask(__name__)
-CORS(app)
-
-# Initialize extensions but don't associate them with the app yet
-db = SQLAlchemy()
-
-# Apply configuration
-from .config import Config  # Ensure config is in the same directory or properly referenced
-app.config.from_object(Config)
-print(app.config['SQLALCHEMY_DATABASE_URI'])  # This should print the URI if set
-
-
-# Setup CORS
-CORS(app, resources={r"/*": {"origins": "http://localhost:8081"}})
-
-# Import blueprints and models after app and db are defined
-from .auth import auth_bp  # Assuming auth_bp is defined in the auth module
-
-# Register blueprints
-app.register_blueprint(auth_bp)
-
-# Initialize SQLAlchemy with the app
->>>>>>> 877966aa5120b70c87fb43d591acbba184ed2ef3
 db.init_app(app)
 
 # Assuming that views and models modules don't cause circular imports
